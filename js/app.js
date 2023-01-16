513746f15ffa000f42c33d065435d7cdc91204bd
Heka9 <90552593+Heka9@users.noreply.github.com>
2023-01-16 14:05:27 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -419,6 +419,17 @@
         function addTouchClass() {
             if (isMobile.any()) document.documentElement.classList.add("touch");
         }
+        function fullVHfix() {
+            const fullScreens = document.querySelectorAll("[data-fullscreen]");
+            if (fullScreens.length && isMobile.any()) {
+                window.addEventListener("resize", fixHeight);
+                function fixHeight() {
+                    let vh = .01 * window.innerHeight;
+                    document.documentElement.style.setProperty("--vh", `${vh}px`);
+                }
+                fixHeight();
+            }
+        }
         let _slideUp = (target, duration = 500, showmore = 0) => {
             if (!target.classList.contains("_slide")) {
                 target.classList.add("_slide");
@@ -2740,6 +2751,7 @@ PERFORMANCE OF THIS SOFTWARE.
         isWebp();
         addTouchClass();
         menuInit();
+        fullVHfix();
         spollers();
         headerScroll();
     })();
